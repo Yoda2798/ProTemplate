a7b8ec2fe1c3cc9567c3a961297eb57485a6d322
Stephen <stephengalligan@live.co.uk>
2021-05-26 23:38:40 +0100

    Added Ice Lolly Relic

---
+++ src/main/java/YodasMod/relics/IceLolly.java
@@ -0,0 +1,36 @@
+package YodasMod.relics;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import static YodasMod.YodasMod.makeID;
+
+public class IceLolly extends AbstractEasyRelic {
+    public static final String ID = makeID("IceLolly");
+    public boolean trigger = false;
+
+    public IceLolly() {
+        super(ID, RelicTier.UNCOMMON, LandingSound.FLAT);
+    }
+
+    public void onPlayerEndTurn() {
+        this.trigger = (EnergyPanel.totalCount > 0);
+    }
+
+    public void atTurnStart() {
+        if (this.trigger) {
+            this.flash();
+            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            this.addToBot(new GainEnergyAction(1));
+        }
+    }
+
+    public void onVictory() {
+        this.trigger = false;
+    }
+}
