b7c525e820d21f05b4c5e844e315161764840ac7
Stephen <stephengalligan@live.co.uk>
2021-05-26 01:02:51 +0100

    Added Propeller Relic

---
+++ src/main/java/YodasMod/relics/Propeller.java
@@ -0,0 +1,41 @@
+package YodasMod.relics;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import static YodasMod.YodasMod.makeID;
+
+public class Propeller extends AbstractEasyRelic {
+    public static final String ID = makeID("Propeller");
+    private static final int TURN_ACTIVATION = 4;
+
+    public Propeller() {
+        super(ID, RelicTier.UNCOMMON, LandingSound.FLAT);
+    }
+
+    public void atBattleStart() {
+        this.counter = 0;
+    }
+
+    public void atTurnStart() {
+        if (!this.grayscale) {
+            ++this.counter;
+        }
+
+        if (this.counter == 4) {
+            this.flash();
+            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            this.addToBot(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, 22));
+            this.counter = -1;
+            this.grayscale = true;
+        }
+
+    }
+
+    public void onVictory() {
+        this.counter = -1;
+        this.grayscale = false;
+    }
+}
