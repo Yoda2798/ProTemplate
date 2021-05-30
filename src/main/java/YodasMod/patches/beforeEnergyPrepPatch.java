c4dada1bc9bb5d70ea027e66416cb7edf2608a70
Stephen <stephengalligan@live.co.uk>
2021-05-30 23:44:17 +0100

    Removed unnecessary code

--- src/main/java/YodasMod/patches/beforeEnergyPrepPatch.java
+++ src/main/java/YodasMod/patches/beforeEnergyPrepPatch.java
@@ -7,16 +7,12 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import javassist.CtBehavior;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SpirePatch2(
         clz = AbstractPlayer.class,
         method = "preBattlePrep"
 )
 public class beforeEnergyPrepPatch {
-
-    private static final Logger logger = LogManager.getLogger(beforeEnergyPrepPatch.class.getName());
 
     @SpireInsertPatch(
             locator = Locator.class
