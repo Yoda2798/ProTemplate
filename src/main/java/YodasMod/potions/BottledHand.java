e6d8717687d2d4bce1285f49cd49c776e7cdb5b9
Stephen <stephengalligan@live.co.uk>
2021-05-30 01:38:36 +0100

    Added Bottled Hand Potion

---
+++ src/main/java/YodasMod/potions/BottledHand.java
@@ -0,0 +1,54 @@
+package YodasMod.potions;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.GameDictionary;
+import com.megacrit.cardcrawl.helpers.PowerTip;
+import com.megacrit.cardcrawl.helpers.TipHelper;
+import com.megacrit.cardcrawl.localization.PotionStrings;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+import com.megacrit.cardcrawl.powers.watcher.BlockReturnPower;
+
+import static YodasMod.YodasMod.makeID;
+
+public class BottledHand extends AbstractEasyPotion {
+    public static final String POTION_ID = makeID("BottledHand");
+    private static final PotionStrings potionStrings = CardCrawlGame.languagePack.getPotionString(POTION_ID);
+    public static final String NAME = potionStrings.NAME;
+    public static final String[] DESCRIPTIONS = potionStrings.DESCRIPTIONS;
+
+    public static final Color liquidColor = Color.CYAN.cpy();
+    public static final Color hybridColor = null;
+    public static final Color spotsColor = null;
+
+    public static final AbstractPlayer.PlayerClass playerClass = null;
+
+    public BottledHand() {
+        super(NAME, POTION_ID, PotionRarity.UNCOMMON, PotionSize.S, PotionColor.NONE, playerClass);
+        this.isThrown = true;
+        this.targetRequired = true;
+    }
+
+    @Override
+    public void initializeData() {
+        this.potency = this.getPotency();
+        this.description = DESCRIPTIONS[0] + this.potency + DESCRIPTIONS[1] + this.potency + DESCRIPTIONS[2];
+        this.tips.clear();
+        this.tips.add(new PowerTip(this.name, this.description));
+        this.tips.add(new PowerTip(TipHelper.capitalize(GameDictionary.BLOCK.NAMES[0]), (String)GameDictionary.keywords.get(GameDictionary.BLOCK.NAMES[0])));
+    }
+
+    @Override
+    public void use(AbstractCreature target) {
+        this.addToBot(new ApplyPowerAction(target, AbstractDungeon.player, new BlockReturnPower(target, this.potency), this.potency));
+    }
+
+    @Override
+    public int getPotency(final int potency) {
+        return 3;
+    }
+}
