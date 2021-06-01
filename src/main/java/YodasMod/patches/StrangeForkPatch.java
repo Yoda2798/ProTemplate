cab5ca39d8c6f8646b1f4ebda97b408fb71ec1a1
Stephen <stephengalligan@live.co.uk>
2021-06-01 02:15:35 +0100

    Removed unnecessary code

--- src/main/java/YodasMod/patches/StrangeForkPatch.java
+++ src/main/java/YodasMod/patches/StrangeForkPatch.java
@@ -3,7 +3,6 @@ package YodasMod.patches;
 import YodasMod.relics.StrangeFork;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import javassist.CtBehavior;
