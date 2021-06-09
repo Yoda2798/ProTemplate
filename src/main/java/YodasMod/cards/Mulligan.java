dad878f250130a9f2fbf90048f155da9c6090d3d
Stephen <stephengalligan@live.co.uk>
2021-06-09 02:38:05 +0100

    Fixed Mulligan Card

--- src/main/java/YodasMod/cards/Mulligan.java
+++ src/main/java/YodasMod/cards/Mulligan.java
@@ -4,14 +4,12 @@ import YodasMod.actions.MulliganAction;
 import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import YodasMod.cards.AbstractEasyCard;
 
 import static YodasMod.YodasMod.makeID;
-import static YodasMod.util.Wiz.*;
 
 public class Mulligan extends AbstractEasyCard {
     public final static String ID = makeID("Mulligan");
-    // intellij stuff , self, rare, , , , , 1, 2
+    // intellij stuff , self, rare, , , , , 1, 1
 
     public Mulligan() {
         super(ID, 0, CardType.ATTACK, CardRarity.RARE, CardTarget.SELF, CardColor.GREEN);
@@ -25,6 +23,6 @@ public class Mulligan extends AbstractEasyCard {
     }
 
     public void upp() {
-        upgradeMagicNumber(2);
+        upgradeMagicNumber(1);
     }
 }
