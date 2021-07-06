50a39bc4b88c4c5321790297c5987b9d0173e4b6
Stephen <stephengalligan@live.co.uk>
2021-07-06 23:12:29 +0100

    Added Gumguard relic

---
+++ src/main/java/YodasMod/relics/Gumguard.java
@@ -0,0 +1,28 @@
+package YodasMod.relics;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.EquilibriumPower;
+
+import static YodasMod.YodasMod.makeID;
+
+public class Gumguard extends AbstractEasyRelic {
+    public static final String ID = makeID("Gumguard");
+
+    public Gumguard() {
+        super(ID, RelicTier.UNCOMMON, LandingSound.FLAT);
+    }
+
+    public void atBattleStart() {
+        AbstractPlayer p = AbstractDungeon.player;
+        this.flash();
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new ApplyPowerAction(p, p, new EquilibriumPower(p, 1), 1));
+    }
+
+    public boolean canSpawn() {
+        return !AbstractDungeon.player.hasRelic("Runic Pyramid");
+    }
+}
