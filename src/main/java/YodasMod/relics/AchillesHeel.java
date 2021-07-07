80713702e4b23600c86a5e493206dc5abdbe0670
Stephen <stephengalligan@live.co.uk>
2021-07-07 23:36:55 +0100

    Refactored to use interfaces

--- src/main/java/YodasMod/relics/AchillesHeel.java
+++ src/main/java/YodasMod/relics/AchillesHeel.java
@@ -1,13 +1,32 @@
 package YodasMod.relics;
 
+import com.evacipated.cardcrawl.mod.stslib.relics.OnApplyPowerRelic;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static YodasMod.YodasMod.makeID;
 
-public class AchillesHeel extends AbstractEasyRelic {
+public class AchillesHeel extends AbstractEasyRelic implements OnApplyPowerRelic {
     public static final String ID = makeID("AchillesHeel");
 
     public AchillesHeel() {
         super(ID, RelicTier.UNCOMMON, LandingSound.FLAT, CardColor.GREEN);
+    }
+
+    @Override
+    public int onApplyPowerStacks(AbstractPower powerToApply, AbstractCreature target, AbstractCreature source, int stackAmount) {
+        if (powerToApply.ID.equals(WeakPower.POWER_ID) && target != null && !target.isPlayer) {
+            this.flash();
+            powerToApply.amount++;
+            stackAmount++;
+        }
+        return stackAmount;
+    }
+
+    @Override
+    public boolean onApplyPower(AbstractPower powerToApply, AbstractCreature target, AbstractCreature source) {
+        return true;
     }
 }
--- src/main/java/YodasMod/relics/BrassKnuckles.java
+++ src/main/java/YodasMod/relics/BrassKnuckles.java
@@ -1,13 +1,32 @@
 package YodasMod.relics;
 
+import com.evacipated.cardcrawl.mod.stslib.relics.OnApplyPowerRelic;
 import com.megacrit.cardcrawl.cards.AbstractCard.CardColor;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static YodasMod.YodasMod.makeID;
 
-public class BrassKnuckles extends AbstractEasyRelic {
+public class BrassKnuckles extends AbstractEasyRelic implements OnApplyPowerRelic {
     public static final String ID = makeID("BrassKnuckles");
 
     public BrassKnuckles() {
         super(ID, RelicTier.UNCOMMON, LandingSound.FLAT, CardColor.RED);
+    }
+
+    @Override
+    public int onApplyPowerStacks(AbstractPower powerToApply, AbstractCreature target, AbstractCreature source, int stackAmount) {
+        if (powerToApply.ID.equals(VulnerablePower.POWER_ID) && target != null && !target.isPlayer) {
+            this.flash();
+            powerToApply.amount++;
+            stackAmount++;
+        }
+        return stackAmount;
+    }
+
+    @Override
+    public boolean onApplyPower(AbstractPower powerToApply, AbstractCreature target, AbstractCreature source) {
+        return true;
     }
 }
